4d8999c4142225d7bcf95124d9465b34d48383c5
ibrahim <6761162@gmail.com>
2020-05-11 13:17:42 +0300

    tamamlandi

--- proje/hosgeldiniz.php
+++ proje/hosgeldiniz.php
@@ -12,6 +12,7 @@ echo <<<EOF
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>İAU | Hoşgeldiniz </title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
